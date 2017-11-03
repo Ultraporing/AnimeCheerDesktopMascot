beedd42a2c496906a547bffa073ac4fa35e10c0d
Tobias Lenz <Black_D@gmx.de>
2017-11-03 22:44:57 +0100

    init

---
+++ AnimeCheerDesktopMascot/AnimeCheerDesktopMascotWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimeCheerDesktopMascot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class AnimeCheerDesktopMascotWindow : Window
+    {
+        public double DefaultWidth = 0;
+        public double DefaultHeight = 0;
+
+        public AnimeCheerDesktopMascotWindow()
+        {
+            InitializeComponent();
+            DefaultWidth = Width;
+            DefaultHeight = Height;
+            App.AnimeCheerDesktopMascotWindow = this;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                if (Properties.Settings.Default.LockWindow)
+                {
+                    return;
+                }
+
+                this.DragMove();
+                Properties.Settings.Default.LastWindowPos = new System.Drawing.Point((int)Left, (int)Top);
+                Properties.Settings.Default.Save();
+            }
+                
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Topmost = Properties.Settings.Default.Topmost;
+
+            if (Properties.Settings.Default.FirstRun)
+            {
+                var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+
+                if (Properties.Settings.Default.HalfSize)
+                {
+                    this.Width /= 2;
+                    this.Height /= 2;
+                }
+
+                this.Left = desktopWorkingArea.Right - this.Width;
+                this.Top = desktopWorkingArea.Bottom - this.Height;
+
+                Properties.Settings.Default.FirstRun = false;
+                Properties.Settings.Default.LastWindowPos = new System.Drawing.Point((int)Left, (int)Top);
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                this.Left = Properties.Settings.Default.LastWindowPos.X;
+                this.Top = Properties.Settings.Default.LastWindowPos.Y;
+            }
+        }
+
+        private void CheerImage_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Properties.Settings.Default.MouseoverTransparency)
+            {
+                CheerImage.Opacity = 0.5;
+            }
+        }
+
+        private void CheerImage_MouseLeave(object sender, MouseEventArgs e)
+        {
+            CheerImage.Opacity = 1;
+        }
+    }
+}
---
+++ AnimeCheerDesktopMascot/App.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace AnimeCheerDesktopMascot
+{
+    public static class Commands
+    {
+        public static readonly RoutedUICommand Topmost = new RoutedUICommand();
+        public static readonly RoutedUICommand Exit = new RoutedUICommand();
+        public static readonly RoutedUICommand MouseoverTransparency = new RoutedUICommand();
+        public static readonly RoutedUICommand LockWindow = new RoutedUICommand();
+        public static readonly RoutedUICommand HalfSize = new RoutedUICommand();
+    }
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static AnimeCheerDesktopMascotWindow AnimeCheerDesktopMascotWindow = null;
+        public static bool CheckMalal = true;
+
+        public App()
+        {
+            InitializeComponent();
+            var topmostBinding = new CommandBinding(Commands.Topmost, TopmostExecuted, CommandCanExecute);
+            var mouseoverTransparencyBinding = new CommandBinding(Commands.MouseoverTransparency, MouseoverTransparencyExecuted, CommandCanExecute);
+            var lockwindowBinding = new CommandBinding(Commands.LockWindow, LockWindowExecuted, CommandCanExecute);
+            var exitBinding = new CommandBinding(Commands.Exit, ExitExecuted, CommandCanExecute);
+            var halfsizeBinding = new CommandBinding(Commands.HalfSize, HalfSizeExecuted, CommandCanExecute);
+            CommandManager.RegisterClassCommandBinding(typeof(Popup), topmostBinding);
+            CommandManager.RegisterClassCommandBinding(typeof(Popup), exitBinding);
+            CommandManager.RegisterClassCommandBinding(typeof(Popup), mouseoverTransparencyBinding);
+            CommandManager.RegisterClassCommandBinding(typeof(Popup), lockwindowBinding);
+            CommandManager.RegisterClassCommandBinding(typeof(Popup), halfsizeBinding);
+        }
+
+        private void HalfSizeExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (AnimeCheerDesktopMascot.Properties.Settings.Default.HalfSize)
+            {
+                AnimeCheerDesktopMascotWindow.Width /= 2;
+                AnimeCheerDesktopMascotWindow.Height /= 2;
+            }
+            else
+            {
+                AnimeCheerDesktopMascotWindow.Width = AnimeCheerDesktopMascotWindow.DefaultWidth;
+                AnimeCheerDesktopMascotWindow.Height = AnimeCheerDesktopMascotWindow.DefaultHeight;
+            }
+
+            AnimeCheerDesktopMascot.Properties.Settings.Default.Save();
+        }
+
+        private void LockWindowExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            AnimeCheerDesktopMascot.Properties.Settings.Default.Save();
+        }
+
+        private void MouseoverTransparencyExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (!AnimeCheerDesktopMascot.Properties.Settings.Default.MouseoverTransparency)
+            {
+                AnimeCheerDesktopMascotWindow.CheerImage.Opacity = 1;
+            }
+
+            AnimeCheerDesktopMascot.Properties.Settings.Default.Save();
+        }
+
+        private void CommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void TopmostExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            AnimeCheerDesktopMascotWindow.Topmost = AnimeCheerDesktopMascot.Properties.Settings.Default.Topmost;
+            AnimeCheerDesktopMascot.Properties.Settings.Default.Save();
+        }
+
+        private void ExitExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Console.WriteLine("Exit");
+            Shutdown();
+        }
+    }
+}
